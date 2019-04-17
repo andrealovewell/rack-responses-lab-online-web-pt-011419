e815a1f134c1a39c1c026c80f665ee4ab7ffde1c
Andrea Lovewell <andy.lovewell@gmail.com>
2019-04-17 03:24:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,10 +3,11 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    if Time.now.hour <12
+    if Time.now.hour < 12
       resp.write "Good Morning"
     else
       resp.write "Good Aternoon"
     end
+   resp.finish 
   end
 end    
